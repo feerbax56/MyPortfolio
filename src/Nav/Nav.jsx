b6d7bcad4c39daeedb848011e8243afbcf94e607
ÐAnton Evseev <soki1997@mail.ru>
2022-11-29 16:07:23 +0500

    add Nav and Header

---
+++ src/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Nav.module.css'
+const Nav = () => {
+    return (
+        <div className={s.nav}>
+            <a href={''}>1</a>
+            <a href={''}>213</a>
+            <a href={''}>3</a>
+            <a href={''}>4</a>
+            <a href={''}>5</a>
+        </div>
+    );
+};
+
+export default Nav;
